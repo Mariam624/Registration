253f48ddd28e671e8ba12e31668d7a3b4b89f2b5
vahe.h.x@gmail.com <hyanmariam0@gmail.com>
2024-01-10 23:45:11 +0400

    Add payment system

--- app/src/main/java/com/example/registration/ItemActivity.kt
+++ app/src/main/java/com/example/registration/ItemActivity.kt
@@ -1,8 +1,12 @@
 package com.example.registration
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.registration.databinding.ActivityItemBinding
+import com.razorpay.Checkout
+import org.json.JSONObject
 
 class ItemActivity : AppCompatActivity() {
     private var _binding: ActivityItemBinding? = null
@@ -11,10 +15,45 @@ class ItemActivity : AppCompatActivity() {
         _binding=ActivityItemBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        Checkout.preload(applicationContext)
+        val co = Checkout()
+        // apart from setting it in AndroidManifest.xml, keyId can also be set
+        // programmatically during runtime
+        co.setKeyID("rzp_test_DYDdEcFlzbvI9H")
 
         binding.itemListTitle.text=intent.getStringExtra("itemTitle")
         binding.itemListText.text=intent.getStringExtra("itemDesc")
+        binding.itemButtonBuy.setOnClickListener {
+            startPayment()
+        }
 
+    }
+    private fun startPayment() {
+        /*
+        *  You need to pass the current activity to let Razorpay create CheckoutActivity
+        * */
+        val activity: Activity = this
+        val co = Checkout()
+
+        try {
+            val options = JSONObject()
+            options.put("name","Mar624")
+            options.put("description","Demoing Charges")
+            //You can omit the image option to fetch the image from the dashboard
+            options.put("image","https://s3.amazonaws.com/rzp-mobile/images/rzp.jpg")
+            options.put("theme.color", "#53436E");
+            options.put("currency","AMD");
+            options.put("order_id", "order_DBJOWzybf0sJbb");
+            options.put("amount","10000")//pass amount in currency subunits
+
+
+
+
+            co.open(activity,options)
+        }catch (e: Exception){
+            Toast.makeText(activity,"Error in payment: "+ e.message,Toast.LENGTH_LONG).show()
+            e.printStackTrace()
+        }
     }
     override fun onDestroy() {
         super.onDestroy()
--- app/src/main/java/com/example/registration/ItemsActivity.kt
+++ app/src/main/java/com/example/registration/ItemsActivity.kt
@@ -15,9 +15,9 @@ class ItemsActivity : AppCompatActivity() {
         _binding=ActivityItemsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val items= arrayListOf<Item>()
-        items.add(Item(1,"sofa","title1", "descrription1","text1", 200))
-        items.add(Item(2,"chandelier","title2", "descrription2","text2", 100))
-        items.add(Item(3,"furniture","title3", "descrription3","text3", 500))
+        items.add(Item(1,"sofa","Sofa", "A sofa designed to redefine the way you lounge.","The upholstery comes in a wide range of fabrics and leathers with special couture details: decorative, matching or contrasting blanket stitching for the fabric versions, and elegant “envelope stitching” for the leather ones. The feet are either covered in fabric or leather or available in a shellac finish.", 200))
+        items.add(Item(2,"chandelier","Chandelier", " The spherical orb shade, highly mirrored and perfectly reflective, reveals a multiplicity of internal reflections from the integral LED when switched on. ","Brighten up your room with Cooper chandelier that has black and gold finish and offers 6 luminous lights. It has contemporary and modern style that looks good in any concept of your house.", 100))
+        items.add(Item(3,"furniture","Furniture", "descrription3","text3", 500))
 
         binding.itemsList.layoutManager=LinearLayoutManager(this)
         binding.itemsList.adapter=ItemsAdapter(items,this)
